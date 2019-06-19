cefd022f7fab511b2b1ce682343a1ab9b948c3a5
unknown <amandalragone@gmail.com>
2019-06-18 19:31:23 -0500

    Added the initial files Osei created for us.

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# Project-2
-Gift-Scribe
+GiftScribe - An app to track gift ideas for fiends and family.
+
+Layout for this app should be done with mobile display as first priority
+
+Functionality - MVP
+
+    * User logs gir ideas that are storin in a sql database
+    * Gifts are categorized by holiday/event and recipient
+    * User is alerted before holiday/event with gift ideas for that person
+    * Queries can be run for specific holidays/events or people
+    * Queries for previous gifts given to people
+    * Amazon or some other retailer API used to search for gifts from the list
+      and return links to items for sale
+
+Functionality - Possible additions
+_ wishlists
+_ track gifts given to user and who they were from
+
+Database Structure
+Name: "giftreg"
+Tables:
+User: name,password,picture,email
+Holiday: Holiday_name,Date,recipient,user
+Recipient: name,relationship,picture,birthday,user
+Gift: description,recipient,holiday,comment,user,gifted
+
+Routes
+Get
+_ show recipients by user
+_ show gift ideas by user(sorted by recipient)
+_ gift ideas with in the next ??timeframe
+Create
+_ Add new gift idea
+_ Add new user
+_ Add new recipient
+_ Add new holiday/event
+Update
+_ Edit gift idea(new comment, mark as gifted)
+_ Edit Recipient data
+_ Edit Holiday/event data
+
+Required webpages
+_ Landing page(login/create new user)
+_ User homepage
+_ modal for adding recipient
+_ modal for adding event
+_ modal for adding gift idea
+_ Recipient page showing list and editing functions \* Page for returing api results
+
+
+Initial DB Models
+
+![DB Models](DB-modeling.jpg);
--- server.js
+++ server.js
@@ -1,35 +1,23 @@
-// =============================================================
-// Dependencies
-// =============================================================
-var express = require("express");
-var path = require("path")
-// =============================================================
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
-app.use(express.static(path.join(__dirname, './app')));
-// Requires the models for syncing
-var db = require('./models');
+const express = require("express");
+const routes = require("./controllers");
+const app = express();
+const PORT = process.env.PORT || 3001;
+var db = require("./models");
 
-// Sets up the Express app to handle data parsing
+// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+app.use(routes);
 
-// Static directory
-app.use(express.static("public"));
-
-// Routes
-// =============================================================
-require("./routes/html-routes.js")(app);
-require("./routes/apiroutes.js")(app);
-
-// *** need to add API routes
-
-// Syncs the sequelize models and starts the Express app
-// =============================================================
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+// Start the API server
+// ADD SEQUELIZE HERE TO CONNECT TO YOUR DB
+db.sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
   });
 });
