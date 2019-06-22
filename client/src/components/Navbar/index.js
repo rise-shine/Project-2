b59c293f2fd9e830df52d0d60747d66566b16dd1
Jake Smith <jacobwsmith19@gmail.com>
2019-06-22 18:06:39 -0500

    Added styling on login page

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -3,16 +3,16 @@ import "../Navbar/Navbar.css";
 
 function Navbar() {
   return (
-        <nav className="navbar navbar-expand-lg navbar-danger bg-danger">
-  <a className="navbar-brand" href="/">Gift Scribe</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon">X</span>
+        <nav className="navbar navbar-expand-lg" id="navbarId">
+  <a className="navbar-brand" id="logo" href="/">Gift Scribe <img id="pencil" src= "https://findicons.com/files/icons/897/nova/128/pencil.png"/></a>
+  <button className="navbar-toggler" id="menuButton" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon">Menu</span>
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item active">
-        <a className="nav-link" href="/">Login <span className="sr-only">(current)</span></a>
+        <a className="nav-link" id="login" href="/">Login <span className="sr-only">(current)</span></a>
       </li>
       <li className="nav-item">
         <a className="nav-link" href="#"></a>
@@ -21,7 +21,7 @@ function Navbar() {
         <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
          My Lists
         </a>
-        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+        <div className="dropdown-menu" id="dropdownId" aria-labelledby="navbarDropdown">
           <a className="dropdown-item" href="/gifts">Gift List</a>
           <a className="dropdown-item" href="/friends">Friend List</a>
           <div className="dropdown-divider"></div>
