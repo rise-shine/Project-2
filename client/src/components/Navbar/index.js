e198e2ba6ff79c1a2f1599ede75103dd30709af9
Mike Garringer <michaelleelyngarringer@gmail.com>
2019-06-22 11:32:02 -0500

    fixed navbar which was accidentally displaying twice

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -3,69 +3,6 @@ import "../Navbar/Navbar.css";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-danger bg-danger">
-      <a className="navbar-brand" href="/">
-        Gift Scribe
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">
-              Home <span className="sr-only">(current)</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Link
-            </a>
-          </li>
-          <li className="nav-item dropdown">
-            <a
-              className="nav-link dropdown-toggle"
-              href="#"
-              id="navbarDropdown"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Dropdown
-            </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a className="dropdown-item" href="/action1">
-                Gift List
-              </a>
-              <a className="dropdown-item" href="/action2">
-                Another action
-              </a>
-              <div className="dropdown-divider" />
-              <a className="dropdown-item" href="/action3">
-                Something else here
-              </a>
-            </div>
-          </li>
-        </ul>
-      </div>
-      ); }
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">
-
-<<<<<<< HEAD
-
-  return (
         <nav className="navbar navbar-expand-lg navbar-danger bg-danger">
   <a className="navbar-brand" href="/">Gift Scribe</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -94,57 +31,6 @@ function Navbar() {
     </ul>
   </div>
 </nav>
-=======
-              Home <span className="sr-only">(current)</span>
-
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Link
-            </a>
-          </li>
-          <li className="nav-item dropdown">
-            <a
-              className="nav-link dropdown-toggle"
-              href="#"
-              id="navbarDropdown"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Dropdown
-            </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a className="dropdown-item" href="/action1">
-                Gift List
-              </a>
-              <a className="dropdown-item" href="/action2">
-                Another action
-              </a>
-              <div className="dropdown-divider" />
-              <a className="dropdown-item" href="/action3">
-                Something else here
-              </a>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
+  )
 }
->>>>>>> f8fcbdba74b4c36f61e72164940815c3e0d7e8b5
-
-<<<<<<< HEAD
-=======
-
-
-<<<<<<< HEAD
 export default Navbar;
-
-
-=======
->>>>>>> 6f34cb558b5065b80ae0ded8074479baa6ee181a
-export default Navbar;
->>>>>>> f8fcbdba74b4c36f61e72164940815c3e0d7e8b5
