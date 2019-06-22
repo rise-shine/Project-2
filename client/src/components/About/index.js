2911d674cef3e53beb311fff78a286657b5e1c32
Mike Garringer <michaelleelyngarringer@gmail.com>
2019-06-22 12:27:23 -0500

    giftlist working again

---
+++ client/src/components/About/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+class about extends React.Component {
+ render(){
+   return(
+    
+<h1>Hello</h1>
+   )
+ } 
+};
+
+export default about;
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -6,7 +6,7 @@ function Navbar() {
         <nav className="navbar navbar-expand-lg navbar-danger bg-danger">
   <a className="navbar-brand" href="/">Gift Scribe</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
+    <span className="navbar-toggler-icon">__</span>
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -22,7 +22,7 @@ function Navbar() {
          My Lists
         </a>
         <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a className="dropdown-item" href="/giftList">Gift List</a>
+          <a className="dropdown-item" href="/gifts">Gift List</a>
           <a className="dropdown-item" href="/friendList">Friend List</a>
           <div className="dropdown-divider"></div>
           <a className="dropdown-item" href="/about">About Us</a>
