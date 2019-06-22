dac4dfc5b8b6b0c9762b3a94a117e0a2e7cf46d3
MAX <47061140+Austxgolfer@users.noreply.github.com>
2019-06-22 12:47:06 -0500

    pulling most recent version

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -3,34 +3,60 @@ import "../Navbar/Navbar.css";
 
 function Navbar() {
   return (
-        <nav className="navbar navbar-expand-lg navbar-danger bg-danger">
-  <a className="navbar-brand" href="/">Gift Scribe</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
+    <nav className="navbar navbar-expand-lg navbar-danger bg-danger">
+      <a className="navbar-brand" href="/">
+        Gift Scribe
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
 
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item active">
-        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#"></a>
-      </li>
-      <li className="nav-item dropdown">
-        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-         My Lists
-        </a>
-        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a className="dropdown-item" href="/giftList">Gift List</a>
-          <a className="dropdown-item" href="/friendList">Friend List</a>
-          <div className="dropdown-divider"></div>
-          <a className="dropdown-item" href="/about">About Us</a>
-        </div>
-      </li>
-    </ul>
-  </div>
-</nav>
-  )
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="/">
+              Home <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#" />
+          </li>
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              My Lists
+            </a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a className="dropdown-item" href="/giftList">
+                Gift List
+              </a>
+              <a className="dropdown-item" href="/friendList">
+                Friend List
+              </a>
+              <div className="dropdown-divider" />
+              <a className="dropdown-item" href="/about">
+                About Us
+              </a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 export default Navbar;
