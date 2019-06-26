85e5c2ddef0896da4b63fc3fdad70e91eacb24d3
unknown <amandalragone@gmail.com>
2019-06-26 00:09:42 -0500

    Added routes to write and read items in the Gifts table.

---
+++ controllers/api/gift.js
@@ -0,0 +1,57 @@
+const router = require("express").Router();
+var db = require("../../models");
+
+// // After the user signs in, the code will read the DB. Then, it will render the friends in the welcome page
+router.get("/list", function(req, res) {
+  
+  db.Gift.findAll()
+  .then(function(response) { 
+    res.json(response);
+    }).catch(err => {
+      console.log(err);
+      
+    });
+});
+
+router.get("/list/:id", function(req, res) {
+  
+  db.Gift.findAll({
+    where: {
+      FriendId: req.params.id
+    },
+    include: [db.Friend]
+  })
+    
+    .then(function(response) { 
+    res.json(response);
+    }).catch(err => {
+      console.log(err);
+      
+    });
+});
+
+// post route to create gifts
+router.post("/create/:id", function(req, res) {
+  const { itemName, comments, price } = req.body;
+  const id = req.params.id;
+
+  console.log(itemName, comments, price, id);
+  
+  db.Gift.create({
+    itemName: itemName,
+    comments: comments,
+    price: price,
+    FriendId: id
+  })
+    // pass the result of our call
+    .then(function(response) {
+
+    res.json(response);
+     
+    }).catch(err => {
+      console.log(err);
+    });
+});
+
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("./user");
 const friendRoutes = require("./friend");
+const giftRoutes = require("./gift");
 
 router.use("/user", userRoutes);
 router.use("/friend", friendRoutes);
+router.use("/gift", giftRoutes);
 
 module.exports = router;
