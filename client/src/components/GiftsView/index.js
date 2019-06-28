8ee102866b4fcbd04f0867dd45ff71af81d47e30
MAX <47061140+Austxgolfer@users.noreply.github.com>
2019-06-27 21:25:26 -0500

    added logic to the giftview index file to display purchased not purchased in the gift table

--- client/src/components/GiftsView/index.js
+++ client/src/components/GiftsView/index.js
@@ -3,35 +3,45 @@ import GiftsList from "../GiftsList";
 import "./giftsview.css";
 
 function GiftsView(props) {
-
   return (
-
     <table className="table">
       <thead>
         <tr className="headings">
-          <th scope="col-2" id="idea">Gift Idea</th>
-          <th scope="col-2" id="comments">Comments</th>
-          <th scope="col-2" id="price">Price</th>
-          <th scope="col-2" id="bought">Have you bought it?</th>
+          <th scope="col-2" id="idea">
+            Gift Idea
+          </th>
+          <th scope="col-2" id="comments">
+            Comments
+          </th>
+          <th scope="col-2" id="price">
+            Price
+          </th>
+          <th scope="col-2" id="bought">
+            Have you bought it?
+          </th>
         </tr>
       </thead>
-         
-      {props.gifts.map(gift => (
-                    
-        <GiftsList
-          key={gift.id}
-          itemName={gift.itemName}
-          comments={gift.comments}
-          price={gift.price}
-          completed={gift.completed}
-        />
-      ))}
-    
 
-      
-
+      {props.gifts.map(gift =>
+        gift.completed === true ? (
+          <GiftsList
+            key={gift.id}
+            itemName={gift.itemName}
+            comments={gift.comments}
+            price={gift.price}
+            completed={"Already Purchased"}
+          />
+        ) : (
+          <GiftsList
+            key={gift.id}
+            itemName={gift.itemName}
+            comments={gift.comments}
+            price={gift.price}
+            completed={"Not Purchased"}
+          />
+        )
+      )}
     </table>
-  )
-    
+  );
 }
 export default GiftsView;
